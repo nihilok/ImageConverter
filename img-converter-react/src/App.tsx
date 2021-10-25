7e6956bb6fa6d1f76a899348e4da3eae6e45b83d
nihilok <mjfullstack@gmail.com>
2021-10-25 23:37:32 +0100

    tweaks/formatting

--- img-converter-react/src/App.tsx
+++ img-converter-react/src/App.tsx
@@ -21,25 +21,30 @@ function App() {
     }
 
     const onFileChange = (event: FormEvent<HTMLInputElement>) => {
-
         const target = event.target as HTMLInputElement
         const file = target.files ? target.files[0] as File : null
-
         setFormState(p => ({
             ...p,
             file: file
         }))
     }
 
+    const parseFilename = () => {
+        let fileName = formState.file?.name ?? 'image.image'
+        let fileNameArr = fileName.split('.')
+            .slice(0, fileName.split('.').length - 1)
+        fileNameArr.push(formState.ext)
+        return fileNameArr.join('.')
+    }
+
     const onSubmit = async (event: FormEvent) => {
         event.preventDefault()
-        let formData = new FormData()
-        console.group("File Submit/Download")
-        console.log(`Before size: ${formState.file?.size}`)
-        formData.append('ext', formState.ext ?? '')
-        if (formState.file) {
-            formData.append('image', formState.file, formState.file.name)
-        }
+        if (!formState.file || !formState.ext.length) return;
+        let formData = new FormData();
+        console.group("File Submit/Download");
+        console.log(`Before size: ${formState.file?.size}`);
+        formData.append('ext', formState.ext ?? '');
+        formData.append('image', formState.file, formState.file.name);
         await fetch(`/convert`, {
             method: 'post',
             body: formData,
@@ -50,11 +55,7 @@ function App() {
                     const a = document.createElement('a');
                     a.style.display = 'none';
                     a.href = url;
-                    let fileName = formState.file?.name ?? 'image.image'
-                    let fileNameArr = fileName.split('.').slice(0, fileName.split('.').length - 1)
-                    fileNameArr.push(formState.ext)
-                    fileName = fileNameArr.join('.')
-                    a.download = fileName;
+                    a.download = parseFilename();
                     document.body.appendChild(a);
                     a.click();
                     document.body.removeChild(a);
@@ -63,30 +64,38 @@ function App() {
                     setFormState(p => ({
                         ...p,
                         ext: 'Invalid File Extension'
-                    }))
+                    }));
                 } else {
-                    console.error(res)
+                    console.error(res);
                 }
             }
         )).catch(err => {
-            console.error(err)
-        })
-        console.groupEnd()
+            console.error(err);
+        });
+        console.groupEnd();
     }
 
     return (
         <div className="App">
             <form onSubmit={onSubmit} className={'form'}>
                 <h1>Convert Image:</h1>
-                <div className='form-control'><label htmlFor={'file'}>Image File: </label><input onChange={onFileChange}
-                                                                                                 type="file" name="file"
-                                                                                                 id="file"/></div>
-                <div className='form-control'><label htmlFor={'ext'}>Extension: </label><input onChange={onChange}
-                                                                                               type="ext" name="ext"
-                                                                                               id="ext"/></div>
+                <div className='form-control'>
+                    <label htmlFor={'file'}>Image File: </label>
+                    <input onChange={onFileChange}
+                           type="file" name="file"
+                           id="file"/>
+                </div>
+                <div className='form-control'>
+                    <label htmlFor={'ext'}>Extension: </label>
+                    <input onChange={onChange}
+                           type="ext" name="ext"
+                           id="ext"/>
+                </div>
                 <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-                    {formState.ext.length > 5 && <small style={{color: 'firebrick'}}>'Invalid File Extension'</small>}
-                    <input type="submit" value="Convert" disabled={formState.ext.length > 5}/></div>
+                    {formState.ext.length > 5 &&
+                    <small style={{color: 'firebrick'}}>'Invalid File Extension'</small>}
+                    <input type="submit" value="Convert" disabled={formState.ext.length > 5}/>
+                </div>
             </form>
         </div>
     );
