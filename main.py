f666cf6bac6891080d41af1beb3281116cfa1a1c
nihilok <mjfullstack@gmail.com>
2021-10-26 16:51:52 +0100

    formatting

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import io
+
 import uvicorn
+
 from fastapi import FastAPI, UploadFile, File, Form, HTTPException
 from fastapi.responses import StreamingResponse
 from fastapi.staticfiles import StaticFiles
 from PIL import Image
+
 
 app = FastAPI()
 
@@ -13,7 +16,7 @@ async def convert_image(ext: str = Form(...), image: UploadFile = File(...)):
     try:
         image_file = image.file.read()
         pil_image = Image.open(io.BytesIO(image_file))
-        pil_image = pil_image.convert("RGB")
+        pil_image = pil_image.convert("RGBA")
         new_size = (int(x * 0.9) for x in pil_image.size)
         pil_image = pil_image.resize(new_size, resample=Image.ANTIALIAS)
         with io.BytesIO() as output:
@@ -25,7 +28,7 @@ async def convert_image(ext: str = Form(...), image: UploadFile = File(...)):
 
 
 app.mount(
-    "/", StaticFiles(directory="./img-converter-react/build", html=True), name="public"
+    "/", StaticFiles(directory="./img-converter-react/build", html=True), name="spa"
 )
 
 if __name__ == "__main__":
